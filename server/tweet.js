8ce0fe5ec26bbebc678e045fb406ed900d1ddc2f
Andy Jiang <andyjiang@gmail.com>
2014-11-18 00:12:16 -0800

    moved twitter abstraction to lib

--- server/tweet.js
+++ server/tweet.js
@@ -3,25 +3,7 @@
  * Module dependencies.
  */
 
-var Twitter = require('twitter-js-client').Twitter;
-
-/**
- * Twitter credentials
- */
-
-var config = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY || '',
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET || '',
-  accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY || '',
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET || '',
-  callBackUrl: 'http://twapchat.herokuapp.com'
-};
-
-/**
- * Setup Twitter client.
- */
-
-var twitter = new Twitter(config);
+var Twitter = require('../lib/twitter.js');
 
 /**
  * Define `Tweet`.
@@ -43,7 +25,7 @@ Tweet.send = function *send(snap) {
       + 'http://twapchat.herokuapp.com/snap/'
       + snap.id
   };
-  // var tweet = yield twitter.postTweet(params, error, success);
+  // var tweet = yield Twitter.postTweet(params, error, success);
   // return tweet.id;
 };
 
@@ -54,7 +36,7 @@ Tweet.send = function *send(snap) {
  */
 
 Tweet.delete = function *delete(tweetId) {
-  twitter.destroyTweet({ id: tweetId }, error, success);
+  Twitter.destroyTweet({ id: tweetId }, error, success);
 };
 
 /**
