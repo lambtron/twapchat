4aa041b1d3f2491b115e1c4b7849bb5d79380442
Andy Jiang <andyjiang@gmail.com>
2014-11-28 13:26:02 -0800

    fixed order of deletion so image still exists

--- server/routes.js
+++ server/routes.js
@@ -32,8 +32,9 @@ Routes.show = function *show(id) {
   if (!snap) return this.body = 'This snap no longer exists!';
   var views = --snap.views;
   Snaps.update({ id: id }, snap);
+  yield render('snap', { snap: snap });
   if (views === 0) destroy(snap);
-  this.body = yield render('snap', { snap: snap });
+  this.body = 'OK';
 };
 
 /**
